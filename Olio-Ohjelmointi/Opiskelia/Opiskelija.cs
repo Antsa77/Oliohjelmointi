df432b35c47bd3b19e58c7510f98e6550220de11
Antsa77 <anton.nousiainen@student.careeria.fi>
2023-04-11 12:49:45 +0300

    Valmis Tehtävä Opiskelija

---
+++ Olio-Ohjelmointi/Opiskelia/Opiskelija.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opiskelija
+{
+    internal class Opiskelija
+    {
+        private string Nimi { get; set; }
+        private string OpiskelijaID { get; set; }
+        private int Opintopisteet { get; set; }
+
+        public Opiskelija(string _nimi, string _id, int _op)
+        {
+            OpiskelijaID = _id;
+            Opintopisteet = _op;
+            Nimi = _nimi;
+        }
+        public void TulostaData()
+        {
+            Console.WriteLine("Opiskelija: ");
+            Console.WriteLine("Nimi: " + Nimi);
+            Console.WriteLine("OpiskelijaID: " + OpiskelijaID);
+            Console.WriteLine("Opintopisteet: " + Opintopisteet);
+            Console.WriteLine();
+        }
+        public void muokkaaopintopistetitä(int i)
+        {
+            Opintopisteet += i;
+
+            if (Opintopisteet < 0)
+            {
+                Opintopisteet = 0;
+            }
+        }
+    }
+}
+
---
+++ Olio-Ohjelmointi/Opiskelia/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Opiskelija
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Opiskelija Opiskelija1 = new Opiskelija("Matti", "TVTP2022", 0);
+
+            Opiskelija1.TulostaData();
+            Opiskelija1.muokkaaopintopistetitä(5);
+            Opiskelija1.TulostaData();
+        }
+    }
+}
