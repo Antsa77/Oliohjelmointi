a72d900d476d47a9106787adec938753303674a8
Antsa77 <anton.nousiainen@student.careeria.fi>
2023-04-17 10:47:24 +0300

    Kiuas harjoitus lisätty

---
+++ Olio-Ohjelmointi/kiuas kt/Kiuas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Kiuas
+{
+    private bool onPäällä;
+    private int lämpötila;
+    private int kosteus;
+
+    public Kiuas()
+    {
+        onPäällä = false;
+        lämpötila = 0;
+        kosteus = 0;
+    }
+    public void KytkePäälle()
+    {
+        onPäällä = true;
+        Console.WriteLine("Kiuas on päällä.");
+    }
+    public void KytkePois()
+    {
+        onPäällä = false;
+        Console.WriteLine("Kiuas on sammutettu.");
+    }
+    public void AsetaKosteus(int uusiKosteus)
+    {
+        kosteus = uusiKosteus;
+        Console.WriteLine("Kiuaksen kosteus on nyt " + kosteus + " prosenttia.");
+    }
+    public void AsetaLämpötila(int uusiLämpötila)
+    {
+        lämpötila = uusiLämpötila;
+        Console.WriteLine("Kiuaksen lämpötila on nyt " + lämpötila + " astetta.");
+    }
+}
---
+++ Olio-Ohjelmointi/kiuas kt/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Kiuas kiuas = new Kiuas();
+
+        kiuas.KytkePäälle();
+        kiuas.AsetaLämpötila(82);
+        kiuas.AsetaKosteus(47);
+        kiuas.AsetaLämpötila(98);
+        kiuas.AsetaKosteus(75);
+        kiuas.KytkePois();
+    }
+}
