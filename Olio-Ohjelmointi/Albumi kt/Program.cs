6788574d9ef0fa04dc05bce567129fa37668188f
Antsa77 <anton.nousiainen@student.careeria.fi>
2023-05-03 10:46:34 +0300

    Albumi tehtävä tehty

---
+++ Olio-Ohjelmointi/Albumi kt/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Albumi albumi = new Albumi("Vastustamaton", "Gettomasa", 1982);
+        albumi.LisääKappale(new Kappale("Vastustamaton", "Gettomasa", 203));
+        albumi.LisääKappale(new Kappale("Rantatontti", "Gettomasa", 357));
+        albumi.LisääKappale(new Kappale("Shamppanjadietillä", "Gettomasa", 258));
+        albumi.LisääKappale(new Kappale("Hyvä me", "Gettomasa", 295));
+        albumi.LisääKappale(new Kappale("Ei leiki", "Gettomasa", 295));
+        albumi.LisääKappale(new Kappale("Ne otti hoon pois", "Gettomasa", 295));
+        albumi.Tulosta();
+
+        Console.ReadLine();
+
+    }
+}
---
+++ Olio-Ohjelmointi/Albumi kt/albumi.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Kappale
+{
+    public string nimi;
+    public string artisti;
+    public int pituus; // sekunteina
+
+    public Kappale(string nimi, string artisti, int pituus)
+    {
+        this.nimi = nimi;
+        this.artisti = artisti;
+        this.pituus = pituus;
+    }
+
+    public override string ToString()
+    {
+        return $"{nimi} ({artisti}) - {pituus / 60}:{pituus % 60:D2}";
+    }
+}
+
+class Albumi
+{
+    public string nimi;
+    public string artisti;
+    public int vuosi;
+    public List<Kappale> kappaleet = new List<Kappale>();
+
+    public Albumi(string nimi, string artisti, int vuosi)
+    {
+        this.nimi = nimi;
+        this.artisti = artisti;
+        this.vuosi = vuosi;
+    }
+
+    public void LisääKappale(Kappale kappale)
+    {
+        kappaleet.Add(kappale);
+    }
+
+    public void Tulosta()
+    {
+        Console.WriteLine($"{nimi} ({artisti}), {vuosi}");
+        foreach (Kappale kappale in kappaleet)
+        {
+            Console.WriteLine(" - " + kappale.ToString());
+        }
+    }
+}
